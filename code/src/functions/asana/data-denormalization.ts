1da0367dc834e1f8f8c4062253c63ab66168aeb4
Patricija Brečko <patricija.brecko@devrev.ai>
2025-09-15 12:51:08 +0200

    Fix task denormalization bug and add warning for non-existing user in Asana

--- code/src/functions/asana/data-denormalization.ts
+++ code/src/functions/asana/data-denormalization.ts
@@ -3,12 +3,29 @@
 // payload shape of external system requests, extracting relevant properties (such as the assignee, name, and notes). 
 
 export function denormalizeTask(item: any, projectId?: string) {
-  return {
-    data: {
-      assignee: item.data?.assignee?.external,
-      name: item.data?.name || '',
-      notes: item.data?.description?.content?.[0] ? item.data?.description?.content?.[0] : '',
-      ...(projectId && { projects: [projectId] }),
-    },
-  };
+  const data: any = {};
+
+  if (item.data?.name) {
+    data.name = item.data?.name || '';
+  }
+
+  if (item.data?.assignee) {
+    data.assignee = item.data.assignee.external || null;
+    if ('external' in item.data.assignee) {
+      data.assignee = item.data.assignee.external;
+    } else {
+      data.assignee = null;
+      console.warn(`Given DevRev user (${item.data.assignee.devrev}) does not exist in Asana.`);
+    }
+  }
+
+  if (item.data?.description) {
+    data.notes = item.data?.description?.content?.[0] || '';
+  }
+  
+  if (projectId) {
+    data.projects = [projectId];
+  }
+
+  return { data };
 }
